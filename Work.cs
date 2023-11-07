27bda1455d41999f68cae120430757f1a769c4e1
«Pavel» <paveligorevich2108@gmail.com>
2023-11-07 16:01:11 +0200

    Code and ReadMe

--- Work.cs
+++ Work.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Введите количество слов в массиве: ");
-        int n = int.Parse(Console.ReadLine()!);
+        Mass();
+    }
 
-        // Создаем массив слов
+    static void Mass()
+    {
+        Console.Write("Введите количество слов в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
         string[] words = new string[n];
 
-        // Ввод слов пользователем
         for (int i = 0; i < n; i++)
         {
             Console.Write("Введите слово " + (i + 1) + ": ");
-            words[i] = Console.ReadLine()!;
+            words[i] = Console.ReadLine();
         }
 
-        // Выводим массив на экран
         Console.WriteLine("\nИсходный массив слов:");
         foreach (string word in words)
         {
             Console.WriteLine(word);
         }
 
-        // Фильтруем массив и создаем новый массив с короткими словами
         string[] filteredWords = FilterWords(words);
 
-        // Выводим отфильтрованный массив
         Console.WriteLine("\nМассив коротких слов (длина <= 3 символов):");
         foreach (string word in filteredWords)
         {
@@ -35,7 +35,6 @@ class Program
         }
     }
 
-    // Фильтрация массива слов по длине
     static string[] FilterWords(string[] words)
     {
         int count = 0;
